db4c8cdfd28e6db8737a2e2ea6baffc708ee5884
EverGreen7112 <34937866+EverGreen7112@users.noreply.github.com>
2019-12-19 23:47:44 +0200

    Upgraded Q6b

--- Intro. comp/hw4/hw4_322522111.py
+++ Intro. comp/hw4/hw4_322522111.py
@@ -26,7 +26,7 @@ def can_win_nim_mem(stacks):
 def canWinNimMemRec(stacks, dictionary):
     if len([0 for stack in stacks if stack != 0]) == 1:
         return True
-    tupStacks = tuple(stacks)
+    tupStacks = tuple(sorted(stacks))
     if tupStacks in dictionary:
         return dictionary[tupStacks]
     for i in range(len(stacks)):
@@ -134,39 +134,12 @@ def comp_ext(s1, s2):
         return False
     if len(s1) is 0 and len(s2) is 0:
         return True
-    pos = 0
-    if len(s2) is 0:
-        if s1[0] is '*':
-            for i in range(len(s1)):
-                if s1[i] == '*':
-                    pos += 1
-                else:
-                    break
-            if pos == len(s1):
-                return True
-        else:
-            return False
-    if s1[0] == '+':
-        return comp_ext(s1[1:], s2[1:])
-    if s1[0] == '*':
-        while s1[pos] == '*':
-            pos += 1
-        if pos == len(s1):
-            return True
-        starFlag = False
-        starPos = 0
-        for i in range(pos,len(s1)):
-            if s1[i] == '*':
-                starFlag = True
-                starPos = i
-                break
-        if starFlag:
-            return comp_ext(s1[pos:starPos], s2[pos-1:starPos-1]) and comp_ext(s1[starPos:], s2[-(len(s1)-starPos):])
-        else:
-            return comp_ext(s1[pos:], s2[-(len(s1)-pos):])
-    if s1[0] == s2[0]:
-        return comp_ext(s1[1:], s2[1:])
-
+    if s1[-1] == '+':
+        return comp_ext(s1[:-1], s2[:-1])
+    if s1[-1] == '*':
+        return comp_ext(s1[:-1], s2[:len(s1)-1])
+    if s1[-1] == s2[-1]:
+        return comp_ext(s1[:-1], s2[:-1])
 ########
 # Tester
 ########
@@ -197,10 +170,10 @@ def test():
         print("Error in had")
 
     contains = lambda L, R: all(R.count(r) <= L.count(r) for r in R)
-    # L = [1, 2, 4, 8, 16]
-    L = [1, 3, 8, 16]
-    R = subset_sum_search(L, 12)
-    if R == None or not sum(R) == 12 or not contains(L, R):
+    L = [1, 2, 4, 8, 16]
+
+    R = subset_sum_search(L, 13)
+    if R == None or not sum(R) == 13 or not contains(L, R):
         print("Error in subset_sum_search")
 
     R = subset_sum_search(L, 32)
@@ -222,29 +195,22 @@ def test():
         print("Error in comp")
 
     if not comp_ext("abc+e", "abcde"):
-        print("Error in comp_ext1")
+        print("Error in comp_ext")
 
     if comp_ext("abc+", "abcdd"):
-        print("Error in comp_ext2")
+        print("Error in comp_ext")
 
     if not comp_ext("abc*d", "abcd"):
-        print("Error in comp_ext3")
+        print("Error in comp_ext")
 
     if not comp_ext("abc*d", "abcddd"):
-        print("Error in comp_ext4")
+        print("Error in comp_ext")
 
     if not comp_ext("a***", "a"):
-        print("Error in comp_ext5")
+        print("Error in comp_ext")
 
     if not comp_ext("abc+d*e", "abcxdzzzzzzzze"):
-        print("Error in comp_ext6")
+        print("Error in comp_ext")
 
     if comp_ext("abc+d*e", "abcdzzzze"):
-        print("Error in comp_ext7")
-n = 4
-L = [3 for i in range(n)]
-t1 = time.perf_counter()
-s = can_win_nim_mem(L)
-t2 = time.perf_counter()
-print(t2-t1)
-print(s)
+        print("Error in comp_ext")
